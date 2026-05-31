c783198f57cbf719797b6cb43f51a175c0b0ab81
LadyDivinee <ladydivineortega14@gmail.com>
2026-05-31 11:23:24 -0700

    Display each object’s speed, radius, color, and on properties

--- test_fan.py
+++ test_fan.py
@@ -8,9 +8,30 @@ def main():
     first_fan.set_radius(10)
     first_fan.set_color("yellow")
     first_fan.set_on(True)
-    # Assign medium speed, radius 5, color blue, and turn it off 
+    # Assign medium speed, radius 5, color blue, and turn it off
     second_fan = Fan()
     second_fan.set_speed(Fan.MEDIUM)
     second_fan.set_radius(5)
     second_fan.set_color("blue")
     second_fan.set_on(False)
+    # Display each object’s speed, radius, color, and on properties
+    print("+----------------------------------------+")
+    print("|          FIRST FAN PROPERTIES          |")
+    print("+----------------------------------------+")
+    print("|  > Speed  :", first_fan.get_speed())
+    print("|  > Radius :", first_fan.get_radius())
+    print("|  > Color  :", first_fan.get_color())
+    print("|  > On     :", first_fan.get_on())
+    print("+----------------------------------------+")
+    print("")
+    print("+----------------------------------------+")
+    print("|        SECOND FAN PROPERTIES           |")
+    print("+----------------------------------------+")
+    print("|  > Speed  :", second_fan.get_speed())
+    print("|  > Radius :", second_fan.get_radius())
+    print("|  > Color  :", second_fan.get_color())
+    print("|  > On     :", second_fan.get_on())
+    print("+----------------------------------------+")
+#run
+if __name__ == "__main__":
+    main()
