6b2f6c01a2c4e8939bd1190b67e704f9f5c03e4a
LadyDivinee <ladydivineortega14@gmail.com>
2026-05-31 11:12:56 -0700

    assign the maximum speed, radius 10, color yellow, and turn it on

--- test_fan.py
+++ test_fan.py
@@ -2,3 +2,9 @@
 from fan import Fan
 #setter method
 def main():
+    # assign the maximum speed, radius 10, color yellow, and turn it on
+    first_fan = Fan()
+    first_fan.set_speed(Fan.FAST)
+    first_fan.set_radius(10)
+    first_fan.set_color("yellow")
+    first_fan.set_on(True)
