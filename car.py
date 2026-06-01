f6d0c2b9bc18e510ca70769fca06ded5e11a1c8d
LadyDivinee <ladydivineortega14@gmail.com>
2026-06-01 00:01:52 -0700

    private members

--- car.py
+++ car.py
@@ -1,4 +1,8 @@
 #class named Car
 class Car:
     #an _ _init_ _ method that accepts the car’s year model and make as arguments
-    def __init__(self, year_model, make):
+    def __init__(self, year_model, make):\
+        #private members
+        self.__year_model = str(year_model)
+        self.__make = str(make)
+        self.__speed = 0
