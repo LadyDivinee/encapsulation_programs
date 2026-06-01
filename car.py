215b24d4f989c8a24e5886d1c7f034041d001363
LadyDivinee <ladydivineortega14@gmail.com>
2026-06-01 00:04:51 -0700

    methods

--- car.py
+++ car.py
@@ -6,3 +6,15 @@ class Car:
         self.__year_model = str(year_model)
         self.__make = str(make)
         self.__speed = 0
+        #methods
+    def accelerate(self):
+        self.__speed += 5
+
+    def brake(self):
+        if self.__speed >= 5:
+            self.__speed -= 5
+        else:
+            self.__speed = 0
+
+    def get_speed(self):
+        return self.__speed
