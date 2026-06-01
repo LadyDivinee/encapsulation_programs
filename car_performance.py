010d2dab151c4087eb4f5bad3417598b694e3b03
LadyDivinee <ladydivineortega14@gmail.com>
2026-06-01 00:33:52 -0700

    header for BMW 2026 performance

--- car_performance.py
+++ car_performance.py
@@ -3,4 +3,9 @@ from car import Car
 def main():
     #car's year model and make
     bmw_car = Car("2026", "BMW")
+    #header for BMW 2026 performance
+    print("+----------------------------------------+")
+    print("|           BMW 2026 SIMULATOR           |")
+    print("+----------------------------------------+")
+    print("  Initial Speed:", bmw_car.get_speed(), "km/h")
 
