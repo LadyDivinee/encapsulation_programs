ae8b8a4712c480aebd8442be129c0c72227d035e
LadyDivinee <ladydivineortega14@gmail.com>
2026-06-02 01:15:23 -0700

    getter method

--- pet.py
+++ pet.py
@@ -17,3 +17,12 @@ class Pet:
 
     def set_age(self, age):
         self.__age = int(age)
+    # getter method
+    def get_name(self):
+        return self.__name
+
+    def get_animal_type(self):
+        return self.__animal_type
+
+    def get_age(self):
+        return self.__age
