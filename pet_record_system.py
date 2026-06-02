a3d8f324f82b0eb0e5396d00f7485b99623128ca
LadyDivinee <ladydivineortega14@gmail.com>
2026-06-02 01:44:19 -0700

    asks the user to enter the name, type, and age of his or her pet

--- pet_record_system.py
+++ pet_record_system.py
@@ -7,3 +7,7 @@ def main():
     print("+----------------------------------------+")
     print("|   LOVE MY PET CLINIC: RECORD SYSTEM    |")
     print("+----------------------------------------+")
+    # asks the user to enter the name, type, and age of his or her pet
+    name_of_pet = input("Enter your pet's name: ")
+    pet_type = input("Specify the type of pet you own: ")
+    pet_age = int(input("Enter your pet's age: "))
