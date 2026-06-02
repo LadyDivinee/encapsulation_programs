14af6b2e82902daefb81626082d1c34ae5b5b55c
LadyDivinee <ladydivineortega14@gmail.com>
2026-06-02 01:41:04 -0700

    title of the system

--- pet_record_system.py
+++ pet_record_system.py
@@ -3,3 +3,7 @@ from pet import Pet
 def main():
     # information in Pet class
     cute_pet = Pet()
+    # title of the system
+    print("+----------------------------------------+")
+    print("|   LOVE MY PET CLINIC: RECORD SYSTEM    |")
+    print("+----------------------------------------+")
