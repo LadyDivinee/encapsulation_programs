6f8670bd6e68c50e06dfbbebbae994f1e6674546
LadyDivinee <ladydivineortega14@gmail.com>
2026-06-02 01:45:40 -0700

    data should be stored as the object’s attributes using mutators

--- pet_record_system.py
+++ pet_record_system.py
@@ -11,3 +11,8 @@ def main():
     name_of_pet = input("Enter your pet's name: ")
     pet_type = input("Specify the type of pet you own: ")
     pet_age = int(input("Enter your pet's age: "))
+    # data should be stored as the object’s attributes using mutators
+    cute_pet.set_name(name_of_pet)
+    cute_pet.set_animal_type(pet_type)
+    cute_pet.set_age(pet_age)
+    print("")
